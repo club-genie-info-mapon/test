da4d204912bb421d5da35f74253dbb95a93e6f29
ThomasBisimwa138 <thomasbisimwa@gmail.com>
2025-05-12 12:50:31 +0200

    button blue

--- helloworld.py
+++ helloworld.py
@@ -7,7 +7,7 @@ app.geometry('300x300')
 label = Label(app, text="Hello World", font=('Arial', 16))
 label.pack()
 
-button = Button(app, text="Click me", background="red", foreground="white")
+button = Button(app, text="Click me", background="blue", foreground="black")
 button.pack()
 
 app.mainloop()
