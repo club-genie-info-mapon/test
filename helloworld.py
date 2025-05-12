41e5714491cdbf11f1bc9f63a97483dd45334b3a
Tardigrade-droid <percumbuku@gmail.com>
2025-05-12 11:30:11 +0100

    Tkinter Hello World App

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,13 @@
-print('Hello, World!')
+from tkinter import *
+
+app = Tk()
+app.title('My App')
+app.geometry('300x300')
+
+label = Label(app, text="Hello World", font=('Arial', 16))
+label.pack()
+
+button = Button(app, text="Click me", background="red", foreground="white")
+button.pack()
+
+app.mainloop()
